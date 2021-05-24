d8572eb56c688accd18aa068027d3e5513357f10
felype-carvalho <lype.carvalho09@hotmail.com>
2021-05-24 01:36:43 -0300

    Correção na documentação

--- doc/homologacao-script.sql
+++ doc/homologacao-script.sql
@@ -1,20 +1,18 @@
-CREATE DATABASE homologacao;
-
-USE homologacao;
-
 CREATE TABLE produto (id int auto_increment primary key, 
                       nome varchar(80) not null,
                       descricao varchar(120),
-                      preco decimal(10,2) not null,
-                      qtd int not null);
+                      qtd int not null,
+                      preco decimal(10,2));
 
 CREATE TABLE cliente (id int auto_increment primary key, 
                       nome varchar(80) not null,
                       cpf varchar(14) not null,
                       numero_telefone varchar(14),
+                      email varchar(60) not null,
                       data_nascimento date,
                       estado_civil varchar(20),
                       endereco varchar(90) not null,
+                      complemento varchar(120) not null,
                       cep varchar(9) not null,
                       cidade varchar(80) not null,
                       uf varchar(2));
